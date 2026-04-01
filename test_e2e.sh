f9f603987f1a7d8ddcad04bd29e72c5b4160f3f2
Tim Schäfer <tim.schaefer@ae.mpg.de>
2026-04-01 14:25:33 +0200

    NEW: add info howto run individual test

--- test_e2e.sh
+++ test_e2e.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 #
 # Finish dev installation before running this, and have sll services running via './run_dev_nginx_both.bash' in another terminal before starting this...
+#
+# You can run individual tests via:
+#
+#  cd frontend/
+#  npx playwright test tests/e2e/your_test_file.spec.ts
+#
 
 echo "Running E2E tests"
 echo "IMPORTANT: Make sure all services are running via './run_dev_nginx_both.bash' in another terminal before starting this..."
