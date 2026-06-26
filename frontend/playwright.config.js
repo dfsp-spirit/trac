a0ab11b5b6d948df2be1e78c7781648b50b15a6b
Tim Schäfer <tim.schaefer@ae.mpg.de>
2026-06-26 12:35:55 +0200

    NEW: limit e2e tests to 1 worker to prevent crosstalk/same test running at once in 2 browsers

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -2,14 +2,14 @@ const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests/e2e',
-  timeout: 60_000,
+  timeout: 40_000,
   expect: {
     timeout: 10_000,
   },
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 5,
+  workers: process.env.CI ? 1 : 1,
   reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://127.0.0.1:3000/report/',
