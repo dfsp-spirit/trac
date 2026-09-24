3e43a5723280d7595ac5791aae2f683bcb8f4c62
Tim Schaefer <ts+code@rcmd.org>
2026-09-24 19:13:29 +0200

    CHG: disable template copying mechanism by default in frontend settings

--- frontend/src/settings/tud_settings.js
+++ frontend/src/settings/tud_settings.js
@@ -4,7 +4,7 @@ const TUD_SETTINGS = {
   API_BASE_URL: 'http://localhost:8000/tud_backend/api',
   DEFAULT_STUDY_NAME: 'default',
   DEFAULT_STUDIES_FILE: 'settings/studies_config.json',
-  TEMPLATE_ENABLED: true, // Copy Days: enable/disable template feature
+  TEMPLATE_ENABLED: false, // Copy Days: enable/disable template feature
   SHOW_COPY_FROM_BUTTON: false, // Copy Days: show "Copy from..." button (default off)
   IMPRINT_URL: 'https://www.aesthetics.mpg.de/en/imprint.html', // e.g. 'https://example.org/imprint', set to null to hide
   PRIVACY_URL:
